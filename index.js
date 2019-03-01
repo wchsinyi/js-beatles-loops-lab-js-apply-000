431018f89300b201d6baf9d6f1e7878efc53b2d3
happy-quotation-9067 <hsinyi.wc.uma@gmail.com>
2019-03-01 22:45:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // add solution here
+
+
+function theBeatlesPlay(mus, instru){
+  let l=mus.length;
+  let k= [];
+  for (let counter=0; counter<l; counter++){
+    k.push( 'name plays instru'.replace('name', mus).replace('instru', instru))
+  }
+  return k 
+}
+
+function johnLennonFacts(arr){
+  let l = arr.length; 
+  let counter = 0;
+  while (counter< l){
+    arr[counter]=arr[counter]+'!!!'
+    counter++
+  }
+  return arr
+}
+
+function iLoveTheBeatles(num){
+  var k=[];
+  
+}
